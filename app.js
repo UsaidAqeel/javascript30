1264049438644ecf2db87c0c08339bb83adda5f1
UsaidAqeel <100064695+UsaidAqeel@users.noreply.github.com>
2022-04-20 23:45:43 +0500

    new

---
+++ app.js
@@ -0,0 +1,103 @@
+
+// JAVA SCIRPT
+
+// Question No 1
+
+// var user = +prompt("Enter the Positive and decimal Number");
+
+// if(user < 0){
+//     alert("Please Enter the value in positive");
+//     document.write("Refresh and enter the number")
+// }
+// if(user > 0){
+// document.write("value : "+user+"<br />")    
+
+// var round = Math.round(user);
+// document.write("Round off value : " + round+"<br />");
+
+// var floor = Math.floor(user);
+// document.write("Floor value : "+floor+"<br />")
+
+// var ceil = Math.ceil(user);
+// document.write("Ciel value : "+ceil);
+
+// }
+
+///////////
+
+// Question No = 2
+
+// var user = +prompt("Enter the Negative and decimal Number");
+
+// if(user > 0){
+//     alert("Please Enter the value in Negative");
+//     document.write("Refresh and enter the number")
+// }
+// if(user < 0){
+// document.write("value : "+user+"<br />")    
+
+// var round = Math.round(user);
+// document.write("Round off value : " + round+"<br />");
+
+// var floor = Math.floor(user);
+// document.write("Floor value : "+floor+"<br />")
+
+// var ceil = Math.ceil(user);
+// document.write("Ciel value : "+ceil+"<br />");
+
+// }
+
+//////////////
+
+// Question No 3
+
+// var num = -4;
+// var method = Math.abs(num);
+// var num1 = 5;
+// var method1 = Math.abs(num1);
+
+
+// document.write("The Absolute value of : "+num+" is "+method);
+// document.write("<br /> The Absolute value of : "+num1+" is "+method1);
+
+// Question No 4 
+
+// var dice1 = Math.floor(Math.random()*6)+1;
+// document.write( "Random dice Value : "+dice1+"<br  />")
+
+///////////////////////////////
+
+// Question No 5
+
+// var toss = Math.floor(Math.random()*2)+1;
+
+// if(toss == 1){
+//     document.write(toss+"<br /> Random coin value is : Tails")
+// }else if(toss == 2){
+//     document.write(toss+"<br /> Random coin value is : Heads")
+// }
+
+///////////////////////////
+
+// Question No 6
+
+// Number between  1 to 100
+
+// var random = Math.floor(Math.random()*100)+1;
+// document.write("Random Number Between 1 to 100 : "+random);
+
+// Question No 7
+
+var user = prompt("Enter your weight in kilogram")
+document.write("The weight of the user is : " +user )
+
+// Question no 8
+
+// var user = +prompt("Enter The number between 1 to 10");
+// var random = Math.floor(Math.random()*10)+1;
+
+// if(user === random){
+//     alert("Con")
+// }else{
+//     alert("try again")
+// }
